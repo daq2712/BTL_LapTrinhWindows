0a0073d8277d562359844b19c4836a9317bac306
daq2712 <dinhanhquan2612@gmail.com>
2026-05-12 22:30:53 +0700

    Delete BTL directory

--- BTL/DormitoryManagement/AppConfig.cs
+++
@@ -1,7 +0,0 @@
-namespace DormitoryManagement;
-
-internal static class AppConfig
-{
-    public const string ConnectionString =
-        @"Server=.\SQLEXPRESS;Database=QuanLyKyTucXa;Trusted_Connection=True;TrustServerCertificate=True;";
-}
